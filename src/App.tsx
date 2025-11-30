190c28a3d2ef4f30488185860a51d795275be30a
[dyad] <rodrigopepe2006@gmail.com>
2025-11-29 23:31:58 -0400

    Reverted all changes back to version 288fd368df1824e2748b004165792df836fd55ea

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,14 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-
-import AuthGuard from "./components/AuthGuard";
-import PageLayout from "./components/PageLayout";
 import Index from "./pages/Index";
 import Scanner from "./pages/Scanner";
 import Exercises from "./pages/Exercises";
 import Missions from "./pages/Missions";
 import Diets from "./pages/Diets";
-import Auth from "./pages/Auth";
-import Onboarding from "./pages/Onboarding";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
-
-const AppRoutes = () => (
-  <PageLayout>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/scanner" element={<Scanner />} />
-      <Route path="/exercises" element={<Exercises />} />
-      <Route path="/missions" element={<Missions />} />
-      <Route path="/diets" element={<Diets />} />
-    </Routes>
-  </PageLayout>
-);
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -36,16 +18,15 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/onboarding" element={<Onboarding />} />
-            <Route element={<AuthGuard />}>
-              <Route path="/*" element={<AppRoutes />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AuthProvider>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/scanner" element={<Scanner />} />
+          <Route path="/exercises" element={<Exercises />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/diets" element={<Diets />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
