68ec3396f64328f29f6f6ef8615021c8d3d97150
[dyad] <rodrigopepe2006@gmail.com>
2025-12-03 20:01:41 -0400

    [dyad] Fixing i18n with Suspense - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { I18nextProvider } from 'react-i18next';
 import i18n from './lib/i18n';
+import SplashScreen from "./pages/SplashScreen.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <I18nextProvider i18n={i18n}>
-    <App />
-  </I18nextProvider>
+  <React.Suspense fallback={<SplashScreen />}>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.Suspense>
 );
